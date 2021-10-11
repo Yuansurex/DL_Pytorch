2910b2bbc1ea8464544f40b52b4ec6e2f59b0829
yuan <374487332@qq.com>
2021-10-11 13:17:58 +0800

    默认变更列表

---
+++ ch3/3. 12 权重衰减.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# @Time: 2021/10/11 13:13
+# @Author: yuansure 
+# @Email: 374487332@qq.com
+# @Function: Talk is cheap, show me the code
+# 应对过拟合常见方法
+
+import torch
+from torch import nn
+from torch.nn import init
+import numpy as np
+import sys
+import matplotlib.pylab as plt
+
+sys.path.append("..")
+import d2lzh_pytorch as d2l
+
+print("")
+
+print("")
+
+print("")
